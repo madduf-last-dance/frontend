e614ba96f9935e10d00ee478260d5c638ebb1c8d
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2024-05-11 14:02:33 +0200

    Feat: new login page (#2)
    * feat: added front page
    
    * feat: added new login page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from './components/header/header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RandomHomeImageComponent from './components/home-image/random-home-image';
-import p1 from './components/home-image/home-images/p1.jpg';
+import Login from './components/login/login';
+import { useState } from 'react';
 
 function App() {
+
+  const [accessToken, setAccessToken] = useState('')
+
   return (
     <div className="App">
-
-
+    
     <Router>
       <Routes>
           <Route
@@ -22,6 +25,13 @@ function App() {
               </div>
             }
           />
+          <Route
+            path="/login"
+            element={
+              <Login setAccessToken={setAccessToken}></Login>
+            }
+          >
+          </Route>
       </Routes>
     </Router>
 
