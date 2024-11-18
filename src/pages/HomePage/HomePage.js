27b2fa5e172e3bb49374918cdcf657867cc00aac
Nikola Petrovic <40899687+nikolapetrovic1@users.noreply.github.com>
2024-11-18 17:56:13 +0100

    fix: updated header to change based on loggedin role (#4)

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,25 +8,10 @@ import React, { useState, useEffect } from 'react';
 const { Meta } = Card;
 const { Content } = Layout;
 
-const hotels1 = [
-  {
-    id: "1",
-    name: "Hotel Zimbabve",
-    description: "najjaci hotel koji postoji druze moj da l si lud da ovo omanes",
-    location: "Zmaj Jovina bb",
-    benefits: ["wifi", "Kitchen"],
-    photos: ["https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTE2MjI1MjI0NDQ0MzYzMjM4Mg%3D%3D/original/ae3426d1-fba4-44d4-bed2-690426f25f7a.jpeg?im_w=1440&im_q=highq", "url2"],
-    minimumGuests: 1,
-    maximumGuests: 5,
-    isPerGuest: true,
-  },
-];
-
 
 const HomePage = () => {
 
     const navigate = useNavigate();
-    // const [hotels, setHotels] = useState(hotels1);
     const [hotels, setHotels] = useState([]);
 
     const updateHotels = (newHotels) => {
